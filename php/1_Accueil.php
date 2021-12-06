4afacf8de559d27408f5f75b3112f6f619b814a4
Ressan <46036784+Ressan@users.noreply.github.com>
2021-12-06 14:27:51 +0100

    Setup
    Mise en place de la structure du projet

---
+++ php/1_Accueil.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1_accueil</title>
+</head>
+<body>
+    <h1>Wo</h1>
+    <li>
+    <a href="2_Apropos"> A propos</a>
+    <a href="3_ServiceClient">Service client</a>
+
+    </li>
+</body>
+</html>
