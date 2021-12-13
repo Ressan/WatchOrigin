bef845af63196b04ce95bf48e4a22e026b6850ee
Ressan <46036784+Ressan@users.noreply.github.com>
2021-12-13 01:15:39 +0100

    MAJ

--- php/navbar.php
+++ php/navbar.php
@@ -1,30 +1,50 @@
-    <h1>
-    <a href="..">WO</a>
-    </h1>
-    <li>
-    <a href="2_Apropos.php"> A propos</a>
-    <a href="3_ServiceClient.php">Service client</a>
-    <?php
-    if(isset($_SESSION['Mail'])){
-          
-          ?>
-          <a href="4_profil.php" name="Compte">Mon compte</a>
-    <?php
 
-            if($_SESSION['Mail']  == 'Admin@wo.com'){
+      <div class="containerperso">
+          <a href=".." class="btn">
+          <img class="img-fluid" src="../images/logo-wo.png" alt="Logo-Wo" style=" max-width: 10%;">
+          WO</a> 
+      </div>
+    <nav class="navbar navbar-expand-sm bg-light navbar-dark justify-content-center">
+      <ul class="nav nav-justified">
+        <li class="nav-item">
+          <a class="nav-link" href="2_Apropos.php"> A propos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="3_ServiceClient.php">Service client</a>
+        </li>
+        <?php
+        if(isset($_SESSION['Mail'])){
+              
+              ?>
+        <li class="nav-item">
+          <a class="nav-link" href="4_profil.php" name="Compte">Mon compte</a>
+        </li>
+        <?php
 
-                echo'<a href="pageAdmin.php">interface Admin</a>';
+                if($_SESSION['Mail']  == 'Admin@wo.com'){
+
+        ?>
+        <li class="nav-item">
+          <a class="nav-link" href="pageAdmin.php">interface Admin</a>
+        </li>
+        <?php
+                }
+
+
+            }else{
+        ?>
+        <li class="nav-item">
+          <a class="nav-link" href="5_connexion.php" name="Connexion">Connexion</a>
+        </li>
+        <?php
             }
 
+        ?>
+        </li>
 
-        }else{
-    ?>
-          <a href="5_connexion.php" name="Connexion">Connexion</a>
-    <?php
-        }
-
-    ?>
-    </li>
+      </ul>
+    </nav>
+    
 
         <div class="col-sm-6 col-md-3 item">
                     <h3>Produit</h3>
