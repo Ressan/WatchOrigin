20be272d399ed413a53bc21b3e3520116569d021
Ressan <46036784+Ressan@users.noreply.github.com>
2021-12-14 01:09:09 +0100

    Update 2_Apropos.php

--- php/2_Apropos.php
+++ php/2_Apropos.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-       <?php 
+    <link rel="stylesheet" href="apropos.css">
+    <?php 
     include("head.php")
     ?>
     <title>A Propos | WO - Watch Origin</title>
@@ -11,8 +12,32 @@
     include("navbar.php")
     ?>
     <h2>A propos</h2>
-    <p> Ajouter le contenu d'accueil les valeurs etc</p>
+    <h3>Depuis plus d’un siècle, les montres Watch Origin  accompagnent les explorateurs et ceux qui réussissent partout dans le monde, du haut des plus hautes montagnes aux profondeurs des océans.</h3>
+    <p>Aujourd’hui, Watch Origin  est présente aux événements les plus prestigieux de golf, de voile, de tennis, de course automobile et d’équitation. Watch Origin  réalise une contribution unique et pérenne pour la culture, la science et l’exploration.</p>
+    <br><br>
+    <p>UNE MONTRE ÉCOLOGIQUE QUI RESPECTE SON ENVIRONNEMENT : UNE MONTRE SANS PILE.
+La montre automatique, est par définition une montre totalement mécanique et dotée d'un système de remontage automatique. Autrement dit c'est vos mouvements du poignet qui fournissent l'énergie au mouvement de la montre.
 
+ 
+
+LA CONCEPTION DE LA MONTRE EST RESPECTUEUSE DE L’ENVIRONNEMENT.
+Watch Origin a pour but de respecter l'environnement de par son implication dans le respect des matériaux qui sont essentiels à la fabrication de nos montres.
+Nos montres automatiques sont écologiques de par leur fabrication. En effet, pas de produits polluants utilisés lors de la fabrication. Le mouvement, respectueux de son environnement et composé d'acier ou inox et laiton. Le boitier de la montre aussi est écologique. La montre automatique est entièrement recyclable et ne comporte pas de pile, coûteuse à recycler. 
+
+Watch Origin vous propose bien plus que de simples montres automatiques. Une montre automatique, c'est effectivement une montre sans pile, une montre qui est donc respectueuse de son environnement. Tout comme les montres solaires, les montres automatiques se rechargent seules, mais à l'inverse de la montre solaire, la montre automatique se recharge avec votre propre énergie. Ainsi, durant toute la vie de votre montre, jamais vous ne changerez la pile tout simplement parce qu’il n'y en a pas ! Nos montres automatiques sont donc des écologiques, vertes et respectueuses de l'environnement.
+
+ 
+
+En résumé, une montre automatique est définitivement une montre économique, écologique et durable !
+Chez Watch Origin, nous vous offrons tout cela à prix discount, chez nous, vous serez certain de trouver les meilleurs tarifs de toute l'Europe pour vos montres automatiques !
+La montre automatique n'a que des avantages.
+Alors n'attendez plus, choisissez vite votre montre automatique parmi la collection !
+
+ 
+
+Chez Watch Origin, vous intègrerez un programme fidélité unique. Découvrez le plaisir de porter une montre écologique automatique ! Redécouvrez l'écologie ! 
+
+La montre sans pile écologique : l'essayer c'est l'adopter !</p>
     <?php 
     include("footbar.php"); 
     ?>
